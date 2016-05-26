7c8328c57186ca6b529542271500b964ac28220a
arnav dhamija <arnav.dhamija@gmail.com>
2016-05-26 12:42:16 +0530

    cleaned up directory; getting .moc build errors

--- hello.cpp
+++ hello.cpp
@@ -1,29 +1,22 @@
 #include "hello.h"
 #include <QDebug>
-#include <QCoreApplication>
-#include <KCmdLineArgs>
-#include <KConfigGroup>
-#include <KDesktopFile>
-#include <KDirNotify>
-#include <KGlobalSettings>
-#include <KStandardDirs>
-#include <KGlobal>
-#include <KUrl>
-
 class KIOPluginForMetaData : public QObject
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.kde.kio.slave.hello" FILE "hello.json")
 };
-
 extern "C"
 {
     int Q_DECL_EXPORT kdemain( int argc, char **argv )
     {
       qDebug() << "Entering function";
-    QCoreApplication app(argc, argv);
-    //KComponentData("kio_hello", "kdelibs4");
-    KLocale::global();
+      ////KComponentData instance( "kio_hello" );
+
+      if (argc != 4)
+      {
+        fprintf( stderr, "Usage: kio_hello protocol domain-socket1 domain-socket2\n");
+        exit( -1 );
+      }
       hello slave( argv[2], argv[3] );
       slave.dispatchLoop();
       return 0;
@@ -42,3 +35,5 @@ void hello::get( const QUrl &url )
 
 hello::hello( const QByteArray &pool, const QByteArray &app )
 : SlaveBase( "hello", pool, app ) {}
+
+#include "hello.moc"
