e49b6c1197448fab2bed5a55748475946e5303e6
arnav dhamija <arnav.dhamija@gmail.com>
2016-06-02 00:41:09 +0530

    Updated master with the changes

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,30 @@
+/***************************************************************************
+ *   Copyright (C) 2016 by Arnav Dhamija <arnav.dhamija@gmail.com>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
 #include "hello.h"
 #include <QDebug>
+
+class KIOPluginForMetaData : public QObject
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.kde.kio.slave.hello" FILE "hello.json")
+};
 
 extern "C"
 {
@@ -8,10 +33,10 @@ extern "C"
       qDebug() << "Launching KIO slave.";
       if (argc != 4)
       {
-        fprintf( stderr, "Usage: kio_hello protocol domain-socket1 domain-socket2\n");
-        exit( -1 );
+        fprintf(stderr, "Usage: kio_hello protocol domain-socket1 domain-socket2\n");
+        exit(-1);
       }
-      hello slave( argv[2], argv[3] );
+      hello slave(argv[2], argv[3]);
       slave.dispatchLoop();
       return 0;
     }
@@ -24,8 +49,10 @@ void hello::get(const QUrl &url)
   QByteArray str( "Hello world!\n" );
   data(str);
   finished();
-  qDebug() << "Leaving function.";
+  qDebug() << "Leaving function";
 }
 
 hello::hello(const QByteArray &pool, const QByteArray &app)
 : SlaveBase("hello", pool, app) {}
+
+#include "hello.moc"
--- hello.h
+++ hello.h
@@ -1,3 +1,22 @@
+/***************************************************************************
+ *   Copyright (C) 2016 by Arnav Dhamija <arnav.dhamija@gmail.com>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
 #ifndef HELLO_H
 #define HELLO_H
 
@@ -6,9 +25,9 @@
 /**
   This class implements a Hello World kioslave
  */
-class hello : public KIO::SlaveBase
+class hello : public QObject, public KIO::SlaveBase
 {
-    Q_PLUGIN_METADATA(IID "org.kde.kio.slave.hello" FILE "hello.json");
+    Q_OBJECT
     public:
         hello(const QByteArray &pool, const QByteArray &app);
         void get(const QUrl &url) Q_DECL_OVERRIDE;
